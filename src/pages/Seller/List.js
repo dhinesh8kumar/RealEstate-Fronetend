f6d18f5ea2659e20dbc89f92bb5a25821f4f9fa8
T Dhinesh Kumar <1dhineshkumar2002@gmail.com>
2023-05-15 18:02:47 +0530

    seller api integrated

--- src/pages/Seller/List.js
+++ src/pages/Seller/List.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import Sidebar from "../../components/Ssidebar";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
@@ -10,39 +10,111 @@ export default function List(){
   if(localStorage.getItem('authToken')!== "seller") {
     navigate('/Properties');
   }
+  const [data,setData] = useState([]);
+  const loadPayment = async () => {
+    let id = localStorage.getItem("data");
+    
+    const response = await axios.get('http://localhost:9091/api/sproperty/', {
+      params: {
+        user: id,
+        
+      }
+      
+    })
+    .then(response => {
+      console.log(response.data);
+      setData(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      
+    });
+    
+  };
+  
+  useEffect(()=> {
+    loadPayment();
+
+  },[]);
     return(
         <>
         <Sidebar/>
+        <div className="main">
         <table class="table">
   <thead class="thead-dark">
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+    <th scope="col">Proeprty ID</th>
+                            <th scope="col">Reg No.</th>
+                            <th scope="col">Purpose</th>
+
+                            <th scope="col">Address</th>
+
+                            <th scope="col">property_image</th>
+
+                            <th scope="col">Price</th>
+
+                            <th scope="col">Area</th>
+                            <th scope='col'>Owner Name</th>
+
+                            <th scope="col">Property_doc</th>
+
+                            <th scope="col">Status</th>
+
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+  {data.map((item, index) => {
+      const verify=item.Verify;
+      let statusText;
+      if (verify === -1) {
+          statusText = 'O In process';
+      } else if (verify === 1) {
+          statusText = '✔ Approved';
+      } else {
+          statusText = 'X Denied';
+      }
+
+
+return (
+
+    <tr key={item.property_id}>
+
+        <th scope='row'>{item.id}</th>
+        <td>{item.Reg_no}</td>
+
+
+
+        <td>{item.purpose}</td>
+
+        <td>{item.Address}</td>
+
+        <td>{item.property_image}</td>
+
+        <td>{item.Price}</td>
+
+        <td>{item.Area_Size}sqkm</td>
+
+        <td>{item.Owner_Name}</td>
+
+        <td>{item.property_doc}</td>
+
+        
+
+       <td>{statusText}</td>
+
+
+
+
+
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+
+)
+
+})}
+
   </tbody>
 </table>
+</div>
         </>
     )
 }
