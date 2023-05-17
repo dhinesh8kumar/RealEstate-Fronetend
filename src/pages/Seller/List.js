9d7a67bc6ef048e466db1c2de429a0eef6ce6977
Ramanareddy venkata <ramanareddylpu@gmail.com>
2023-05-17 16:33:26 +0530

    css redesign

--- src/pages/Seller/List.js
+++ src/pages/Seller/List.js
@@ -1,120 +1,135 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import Sidebar from "../../components/Ssidebar";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
-export default function List(){
-    const navigate = useNavigate();
-  if(!localStorage.getItem('Name')){
-    navigate('/');
+
+import Button from "@mui/material/Button";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import axios from "axios";
+export default function List() {
+  const navigate = useNavigate();
+  if (!localStorage.getItem("Name")) {
+    navigate("/");
   }
-  if(localStorage.getItem('authToken')!== "seller") {
-    navigate('/Properties');
+  if (localStorage.getItem("authToken") !== "seller") {
+    navigate("/Properties");
   }
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
   const loadPayment = async () => {
     let id = localStorage.getItem("data");
-    
-    const response = await axios.get('https://realestate-backend-b20k.onrender.com/api/sproperty/', {
-      params: {
-        user: id,
-        
-      }
-      
-    })
-    .then(response => {
-      console.log(response.data);
-      setData(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-      
-    });
-    
+
+    const response = await axios
+      .get("https://realestate-backend-b20k.onrender.com/api/sproperty/", {
+        params: {
+          user: id,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     loadPayment();
-
-  },[]);
-    return(
-        <>
-        <Sidebar/>
-        <div className="main">
+  }, []);
+  return (
+    <>
+      <Sidebar />
+      <div className="main">
         <table class="table">
-  <thead class="thead-dark">
-    <tr>
-    <th scope="col">Proeprty ID</th>
-                            <th scope="col">Reg No.</th>
-                            <th scope="col">Purpose</th>
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Proeprty ID</th>
+              <th scope="col">Reg No.</th>
+              <th scope="col">Purpose</th>
 
-                            <th scope="col">Address</th>
+              <th scope="col">Address</th>
 
-                            <th scope="col">property_image</th>
+              <th scope="col">property_image</th>
 
-                            <th scope="col">Price</th>
+              <th scope="col">Price</th>
 
-                            <th scope="col">Area</th>
-                            <th scope='col'>Owner Name</th>
+              <th scope="col">Area</th>
+              <th scope="col">Owner Name</th>
 
-                            <th scope="col">Property_doc</th>
+              <th scope="col">Property_doc</th>
 
-                            <th scope="col">Status</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => {
+              const verify = item.Verify;
+              let statusText;
+              if (verify === -1) {
+                statusText = "O In process";
+              } else if (verify === 1) {
+                statusText = "✔ Approved";
+              } else {
+                statusText = "X Denied";
+              }
+              const inactive = () => {
+                axios
+                  .put(`http://localhost:9091/inactive`, {
+                    id: item.id,
+                  })
 
-    </tr>
-  </thead>
-  <tbody>
-  {data.map((item, index) => {
-      const verify=item.Verify;
-      let statusText;
-      if (verify === -1) {
-          statusText = 'O In process';
-      } else if (verify === 1) {
-          statusText = '✔ Approved';
-      } else {
-          statusText = 'X Denied';
-      }
+                  .then((response) => {
+                    console.log(response.data);
 
+                    alert("User information updated successfully");
+                  })
 
-return (
+                  .catch((error) => {
+                    console.error(error);
 
-    <tr key={item.property_id}>
+                    alert("Error updating user information");
+                  });
 
-        <th scope='row'>{item.id}</th>
-        <td>{item.Reg_no}</td>
+                window.location.reload();
+              };
+              return (
+                <tr key={item.property_id}>
+                  <th scope="row">{item.id}</th>
+                  <td>{item.Reg_no}</td>
 
+                  <td>{item.purpose}</td>
 
+                  <td>{item.Address}</td>
 
-        <td>{item.purpose}</td>
+                  <td>{item.property_image}</td>
 
-        <td>{item.Address}</td>
+                  <td>{item.Price}</td>
 
-        <td>{item.property_image}</td>
+                  <td>{item.Area_Size}sqkm</td>
 
-        <td>{item.Price}</td>
+                  <td>{item.Owner_Name}</td>
 
-        <td>{item.Area_Size}sqkm</td>
+                  <td>{item.property_doc}</td>
 
-        <td>{item.Owner_Name}</td>
-
-        <td>{item.property_doc}</td>
-
-         
-
-       <td>{statusText}</td>
-
-
-
-
-
-    </tr>
-
-)
-
-})}
-
-  </tbody>
-</table>
-</div>
-        </>
-    )
+                  <td>
+                    {statusText} 
+                    <Button
+                      color="secondary"
+                      onClick={inactive}
+                      size="small"
+                      variant="outlined"
+                      startIcon={<DeleteIcon />}
+                    >
+                      INACTIVE
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
